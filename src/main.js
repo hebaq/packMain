fc2d98a98b58bf14f82637d861d0b06eaf8d8fbe
wll8 <xwdaysme@gmail.com>
2023-12-08 11:35:11 +0800

    refactor: 移除冗余文件

--- src/main.js
+++ src/main.js
@@ -1,29 +1,33 @@
-// import Vue from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/browser.js'
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-Vue = window.Vue
+const ws = process.env.NODE_ENV === `development` ? `ws://127.0.0.1:7788` : undefined
 
 Vue.use(ElementUI);
-
 Vue.config.productionTip = false
-new Sys(`ws://127.0.0.1:7788`)
-  .then(async (main) => {
-    console.log(`sys 初始化完成`)
-    main.form.show()
-    window.main = main
-    window.win = main.win
-    window.vm = new Vue({
-      router,
-      store,
-      render: (h) => h(App),
-    }).$mount(`#app`)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+
+if(window.Sys) {
+  new Sys(ws)
+    .then(async (main) => {
+      console.log(`sys 初始化完成`)
+      main.form.show()
+      window.main = main
+      window.win = main.win
+      window.vm = new Vue({
+        router,
+        store,
+        render: (h) => h(App),
+      }).$mount(`#app`)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+} else {
+  console.error(`请在 main.exe 中加载页面`)
+}
+
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,12 @@
 const { defineConfig } = require(`@vue/cli-service`)
+
 module.exports = defineConfig({
   configureWebpack: {
     devtool: `eval-source-map`,
-    // devtool: `eval-cheap-module-source-map`,
-    // devtool: false,
-    externals: {
-      'vue': 'Vue'
-    }
+    externals: process.env.production ? {
+      '@/browser.js': 'Sys'
+    } : {},
   },
-  // lintOnSave: `warning`,
   lintOnSave: false,
   productionSourceMap: true,
-  // transpileDependencies: true,
 })
