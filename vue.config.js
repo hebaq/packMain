dc65e1bcb2a6ae7024229e3ae18eef40c66dd31c
wll8 <xwdaysme@gmail.com>
2023-12-08 11:39:24 +0800

    refactor: 移除冗余文件

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,6 @@ const { defineConfig } = require(`@vue/cli-service`)
 module.exports = defineConfig({
   configureWebpack: {
     devtool: `eval-source-map`,
-    externals: process.env.production ? {
-      '@/browser.js': 'Sys'
-    } : {},
   },
   lintOnSave: false,
   productionSourceMap: true,
